378d2815c9f7c6968d4102a83131a8d512c18dbd
梦里不知身是客 <tang97155@163.com>
2022-10-02 23:53:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const thisYear = new Date().getFullYear()
 const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
 const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
 const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
-const progressBarOfThisYear = generateProgressBar()
 
 function isZero(x) {
   return x < 10 ? '0' + x : x
@@ -16,15 +15,6 @@ function formatDate(time) {
   const mm = time.getMinutes();
   const s = time.getSeconds();
   return y + '/' + isZero(m) + '/' + isZero(d) + ' ' + isZero(h) + ':' + isZero(mm) + ':' + isZero(s);
-}
-
-function generateProgressBar() {
-  const progressBarCapacity = 30
-  const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
-  const progressBar =
-    '█'.repeat(passedProgressBarIndex) +
-    '▁'.repeat(progressBarCapacity - passedProgressBarIndex)
-  return `{ ${progressBar} }`
 }
 
 const readme = `\
@@ -82,3 +72,5 @@ const readme = `\
 </p>
 \
 `
+
+console.log(readme)
